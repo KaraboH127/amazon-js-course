67c7f04714e4c5f8900d276af5ee554b348747d7
Karabo127 <karabohlabangane@gmail.com>
2026-06-03 10:42:24 +0200

    add backend practice file

---
+++ data/backend-practice.js
@@ -0,0 +1,9 @@
+// To send a message via HTTPS
+const xhr = new XMLHttpRequest();
+
+xhr.addEventListener('load', () => {
+  console.log(xhr.response)
+})
+
+xhr.open('GET', 'https://supersimplebackend.dev/images/apple.jpg');
+xhr.send(); 
