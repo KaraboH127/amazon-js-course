16ee7b13157b8e6ee85e1640174db879492bd158
Karabo127 <karabohlabangane@gmail.com>
2026-06-04 10:57:53 +0200

    Convert loadProducts for XHR to fetch

--- data/products.js
+++ data/products.js
@@ -91,11 +91,33 @@ object3.method();
 
 export let products = [];
 
+export function loadProductsFetch() {
+    const promise = fetch('https://supersimplebackend.dev/products').then((response) => {
+      return response.json()
+    }).then((productsData) => {
+      products = productsData.map((productDetails) => {
+        if (productDetails.type === "clothing") {
+          return new Clothing(productDetails);
+        }
+        return new Product(productDetails);
+    });
+
+    console.log('load products');
+    });
+    
+    return promise;
+  };
+  /*
+  loadProductsFetch().then(() => {
+    console.log('next step')
+  });
+  */
+
 export function loadProducts(func) {
-  const xhr =new XMLHttpRequest()
+  const xhr = new XMLHttpRequest();
 
   xhr.addEventListener('load', () => {
-          products = JSON.parse(xhr.response).map((productDetails) => {
+      products = JSON.parse(xhr.response).map((productDetails) => {
         if (productDetails.type === "clothing") {
           return new Clothing(productDetails);
         }
@@ -105,11 +127,11 @@ export function loadProducts(func) {
     console.log('load products');
 
     func()
-  })
+  });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products')
   xhr.send();
-}
+};
 
 
 /*
